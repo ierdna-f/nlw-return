b5e8a4f5652660e420bc7fa72c16328379bcb3e0
Andrei Cardoso <andrei.cardoso@ifood.com.br>
2022-05-07 20:49:50 -0300

    adding relevant comments

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -10,11 +10,15 @@ export function Widget(){
     <Popover className='absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end'>
       
       <Popover.Panel> 
+        {/* form open after clicking the button */} 
         <WidgetForm/>
       </Popover.Panel>
-      
+
+     {/* mini button, - clicking opens Popover.Panel content */} 
       <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>    
           <ChatTeardropDots className='w-6 h-6'/>
+
+          {/* style to show and hide 'feedback' word */} 
           <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
             <span className='pl-2'></span>
             Feedback
