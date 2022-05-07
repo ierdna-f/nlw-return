43226ae959c8a3bc45b333903e6a8d225afe3176
Andrei Cardoso <andrei.cardoso@ifood.com.br>
2022-05-07 20:50:44 -0300

    update on Steps of WidgetForm

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -14,7 +14,7 @@ export function FeedbackTypeStep({ onFeedbackTypeChanged }: FeedbackTypeStepProp
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2 w-full">
+      <div id="teste" className="flex py-8 gap-2 w-full">
         { Object.entries(feedbackTypes).map(([key, value]) => {
           return (
               <button 
